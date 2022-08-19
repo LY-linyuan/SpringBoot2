77ebe15e5089cd18a186292f67c628f94370ac6b
LY-linyuan <3113714035@qq.com>
2022-08-19 21:57:45 +0800

    springboot2学习中---------------

---
+++ three-sprinfboot-second/src/main/java/com/bjpowernode/ThreeSprinfbootSecondApplication.java
@@ -0,0 +1,13 @@
+package com.bjpowernode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreeSprinfbootSecondApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreeSprinfbootSecondApplication.class, args);
+    }
+
+}
